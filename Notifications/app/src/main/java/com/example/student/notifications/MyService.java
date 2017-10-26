da3a030ed302b60e2e8ccbd8ff4fa9447ee83f49
TYTv <tang.yao.zong@gmail.com>
2017-10-26 09:10:12 +0800

    推播

---
+++ Notifications/app/src/main/java/com/example/student/notifications/MyService.java
@@ -0,0 +1,33 @@
+package com.example.student.notifications;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyService extends FirebaseInstanceIdService {
+    public MyService() {
+    }
+
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+//    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TAG", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+
+}
